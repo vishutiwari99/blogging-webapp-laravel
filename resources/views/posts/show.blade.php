0fae398040b347fde91424fd3cc0ab2eec4cb054
Vaibhav Tiwari <vishutiwari99@gmail.com>
2020-09-17 16:15:24 +0530

    Added edit functinality

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,4 +9,11 @@
 </div>
 <hr>
 <small>Written on {{$post->created_at}}</small>
+<hr>
+<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+{!!Form::open(['action'=>['App\Http\Controllers\PostsController@destroy',$post->id],'method'=>'POST', 'class'=>'pull-right'])!!}
+{{Form::hidden('_method','DELETE')}}
+{{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+{!!Form::close()!!}
 @endsection
